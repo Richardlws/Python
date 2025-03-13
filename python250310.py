acc98d1377c23547d48ee0a7273f3179cf0e5e85
Richardlws <liu.bing66066@hotmail.com>
2025-03-13 11:18:53 +0800

    20250313learning

--- python250310.py
+++ python250310.py
@@ -101,5 +101,103 @@ circle = Circle("red",False,5)
 print(circle.color)
 circle.describe()
 
+
+class Book():
+    def __init__(self,title,author,num_pages):
+        self.title = title
+        self.author = author
+        self.num_pages = num_pages
+    def __str__(self):
+        return f"'{self.title}' by {self.author}"
+    def __eq__(self,other):
+        return (self.title == other.title and self.author == other.author)
+    def __lt__(self,other):
+        return (self.num_pages < other.num_pages)
+    def __add__(self,other):
+        return (self.num_pages + other.num_pages)
+    def __contains__(self, keyword):
+        return keyword in self.title or keyword in self.author
+    def __getitem__(self,key):
+        if key == "title":
+            return self.title
+        if key == "author":
+            return self.author
+        elif key == "num_pages":
+            return self.num_pages
+        else:
+            return f"key {key} was not found"
+book1 = Book("The Hobbit","J.R.R Tolkien",310)
+book2 = Book("Harry Potter and The Philosopher's Stone","J.K. Rowling",223)
+book3 = Book("The Lion,the Witch and the Wardrobe","C.S. Lewis",172)
+
+#print(book1)
+#print(book2)
+#print(book3)
+#print(book1 == book2)
+#print(book1 > book2)
+#print(book1 + book2)
+#print("Rowling" in book2)
+print(book2['audio'])
+
+
+
+
+def add_sprinkles(fun):
+    def wrapper():
+        print("*You add sprinkles 🍓*")
+        fun()
+    return wrapper
+
+@add_sprinkles
+def get_ice_cream():
+    print("Here is your ice cream 🍨")
+
+get_ice_cream()
+
+
+
+import os
+file_path = "test.txt"
+if os.path.exists((file_path)):
+    print(f"The location '{file_path}' exists.")
+else:
+    print(f"The location '{file_path}' doesn't exist.")'
+
+
+
 '''
+
+txt_data = "I like pizza!"
+file_path="output.txt"
+
+with open(file_path,"x") as file:
+    file.write(txt_data)
+    print(f"txt file '{file_path}' was created")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
